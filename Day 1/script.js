f2fa5a6d2cfe19d2b6c313f5223785dc51e8d1d3
jeebanCodes <139693626+JeebanBeura@users.noreply.github.com>
2024-07-15 08:02:30 +0530

    Add files via upload

---
+++ Day 1/script.js
@@ -0,0 +1,43 @@
+// Activity 1:
+// Task 1:
+var number = 12;
+console.log("NUMBER: ",number);
+
+// Task 2:
+let string = "Decoding Life";
+console.log("STRING: ",string);
+
+// Activity 2:
+// Task 3:
+const boolean = true;
+console.log("BOOLEAN: ",boolean);
+
+// Activity 3:
+// Task 4:
+const num = 45;
+const str = "Rohit";
+const bool = false;
+const obj = {
+    name:"Jeeban",
+    skills:"MERN stack"
+}
+const arr = ["Jeeban","Chinu","Rabi"];
+console.log("NUM: ",typeof num);
+console.log("STR: ",typeof str);
+console.log("BOOL: ",typeof bool);
+console.log("OBJ: ",typeof obj);
+console.log("ARR: ",typeof arr);
+
+// Activity 4:
+// Task 5:
+let variable = 63;
+console.log("Initial Value: ",variable);
+variable = 18;
+console.log("Reassign Value: ",variable);
+
+// Activity 5:
+// Task 6:
+const skills = "MERN Stack";
+console.log("Initial Skills: ",skills);
+// skills = "MERN Stack and FullStack"; // This code show Error "TypeError: Assignment to constant variable."
+// console.log("Reassign Skills: "skills);
