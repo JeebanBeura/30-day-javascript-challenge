e05a5abd63f4ab2425fc58b52415e20586534d42
jeebanCodes <139693626+JeebanBeura@users.noreply.github.com>
2024-07-16 08:30:56 +0530

    Add files via upload

---
+++ Day 2/script.js
@@ -0,0 +1,73 @@
+// Activity 1:
+// Task 1:
+let addNum1 = 5;
+let addNum2 = 2;
+let add = addNum1 + addNum2;
+console.log("Add two number is: ",add);
+
+// Task 2:
+let subNum1 = 5;
+let subNum2 = 2;
+let sub = addNum1 - addNum2;
+console.log("Subtract two number is: ",sub);
+
+// Task 3:
+let mulNum1 = 5;
+let mulNum2 = 2;
+let mul = addNum1 * addNum2;
+console.log("Multiply two number is: ",add);
+
+// Task 4:
+let divNum1 = 5;
+let divNum2 = 2;
+let div = addNum1 / addNum2;
+console.log("Divide two number is: ",div);
+
+// Task 5:
+let remNum1 = 5;
+let remNum2 = 2;
+let rem = addNum1 % addNum2;
+console.log("Remainder two number is: ",rem);
+
+// Activity 2:
+// Task 6:
+let addNum = 5;
+addNum += 5;
+console.log("Add: ",addNum);
+
+// Task 7:
+let subNum = 10;
+subNum -= 5;
+console.log("Subtract: ",subNum);
+
+// Activity 3:
+// Task 8:
+let x = 5;
+console.log(x > 8);
+console.log(x < 8);
+
+//Task 9:
+console.log(x >= 8);
+console.log(x <= 8);
+
+//Task 10:
+console.log(x == 8);
+console.log(x == 5);
+console.log(x == "5");
+
+console.log(x === 5);
+console.log(x === "5");
+
+// Activity 4:
+// Task 11:
+let a = 6;
+let b = 3;
+
+console.log(a < 10 && b > 1);
+console.log(x == 5 || b == 5);
+console.log(!(a == b));
+
+// Activity 5:
+// Task 12:
+let age = 20;
+console.log((age > 18) ? "Voteable":"not Voteable")
